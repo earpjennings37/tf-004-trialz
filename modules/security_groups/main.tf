a5ac5b8d8bcb645927bb405845fc5b0b391de3f7
earpjennings <wjford28@gmail.com>
2026-03-10 15:01:08 -0400

    minor sg changes

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -1,6 +1,10 @@
 resource "aws_security_group" "earpz-sg" {
   vpc_id    = var.earpz-vpc-id
+  count = var.earpz_create_sg ? 1 : 0
   name      = "earpz-sg"
+  tags = {
+    Name = join("-", ["earpz", "sg"])
+  }
 }
 variable "earpz-vpc-id" {
   type    = string
@@ -10,6 +14,17 @@ variable "earpz_create_sg" {
   type    = bool
   default = false
 }
+variable "tags" {
+  type = map(string)
+  default = {}
+}
+
 output "earpz_create_sg" {
   value = var.earpz_create_sg
 }
+/*
+### create tagzzz ###
+variable "logging" {
+    type = bool
+    default = true
+*/
